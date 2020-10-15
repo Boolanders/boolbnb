22d326170c53336acfa6383fd386cfef21a62db8
Michael Cinus <michaelcinus@gmail.com>
2020-10-15 19:11:40 +0200

    modiche users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this -> call([
 
-            ImageSeeder::class,
             VisitSeeder::class,
             ImageSeeder::class,
             ApartmentSeeder::class
