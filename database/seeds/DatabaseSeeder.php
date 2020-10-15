52ce3b161b0fe5450b113bec7978bd309d8361ee
Michael Cinus <michaelcinus@gmail.com>
2020-10-15 17:04:44 +0200

    aa

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            
-            ImageSeeder::class,
-            VisitSeeder::class
-        ]);
+        $this->call([]);
     }
 }
