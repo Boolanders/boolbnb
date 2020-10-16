d82559c10f376da73c749ef9db3c79c9c6e28cdc
Davide Carpani <carpani.d@gmail.com>
2020-10-16 10:50:13 +0200

    bridge tables

---
+++ database/migrations/2020_10_16_082726_create_apartment_sponsorship_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentSponsorshipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+            $table->id();
+
+            $table -> integer('apartment_id') -> unsigned();
+            $table -> integer('sponsorship_id') -> unsigned();
+            $table -> date('start_date');
+            $table -> date('end_date');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+}
