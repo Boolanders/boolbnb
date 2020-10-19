00f11e2525ee1944542755831ebe3e1776ef0590
Vincenzo <megnavincenzo97@gmail.com>
2020-10-19 11:59:38 +0200

    Changes

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-<<<<<<< HEAD
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-=======
-
-
-Route::get('/', 'HomeController@index')->name('home');
->>>>>>> 026ac4999f7699ce1d420bb5e818355ae39b34c3
