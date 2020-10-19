b73798d03c33a3051cb41f27220febe38495435a
Michael Cinus <michaelcinus@gmail.com>
2020-10-19 16:44:11 +0200

    heder-fixed, padding-card, create footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.main-layout')
 
 @section('content')
-<div class="container">
+<div class="container body">
     <div class="row justify-content-center">
       <h1 class="col-md-12">Appartamenti in evidenza</h1>
       @foreach ($apts as $apt)
-        <div class="col-md-2">
+        <div class="col-md-4 ">
 
-            <div class="card">
+            <div class="card apartment">
                 <div class="card-header">
                 {{ $apt -> description }}
                 </div>
                 <div class="card-body">
-                  <img src="{{ $apt -> images -> img }}" alt="img">
+                  <img src="https://image.freepik.com/free-vector/two-storey-house_1308-16176.jpg" alt="img">
                 </div>
             </div>
 
