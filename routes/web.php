305a9b5c526b5bc43f77fb7c096939fe85aa185b
Davide Carpani <carpani.d@gmail.com>
2020-10-20 12:21:07 +0200

    route & controller

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,16 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 
-
-
-
-
-
 Route::get('/', 'GuestController@index') -> name('home');
 
+Route::get('/show/{id}', 'LoggedController@show') -> name('apt-show');
 
+Route::get('/create', 'LoggedController@create')->name('apt-create');
 
+Route::post('/create/store', 'LoggedController@store' ) -> name('apt-store');
+
+Route::get('/edit/{id}', 'LoggedController@edit')->name('apt-edit');
+
+Route::post('/update/{id}', 'LoggedController@update' ) -> name('apt-update');
+
+Route::get('/delete/{id}', 'LoggedController@delete') -> name('apt-delete');
