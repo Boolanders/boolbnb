75590c25ecfb3229e88b1040d867d42b966b682f
Michele Curtaz <mcurtaz@gmail.com>
2020-10-21 14:33:01 +0200

    profile css

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,48 @@
 @extends('layouts.main-layout')
 @section('content')
-<div class="container">
+<div id="profile">
+    <div class="container">
+    
+        <div class="row">
+            <div class="col-12 text-center p-3">
+                <a class="btn btn-primary" href="{{route('apt-create') }}">Add Apartment</a>
+            </div>
+        </div>
+        @foreach ($apts as $apt)
+        <div class="media border rounded m-2 p-2 ">
+            @if ($apt -> images -> first -> img -> img)
 
-    <div class="form">
+            <img src=" {{ $apt -> images -> first -> img -> img }} " class="mr-3 center-cropped align-self-center" alt="...">
 
-        <a class="btn btn-lg btn-warning btn-block" href="{{route('apt-create') }}">Add Apartment</a>
-        
-        <div class="list-group">
+            @else
+
+            <img src=" {{ asset('img/image-not-found.png') }} " class="mr-3 center-cropped align-self-center" alt="...">
+
+            @endif
+
+            <div class="media-body">
+              <h4 class="mt-0"> {{ $apt -> name }} </h4>
+              <div class="txt">
+                  {{ $apt -> description }}
+              </div>
+            </div>
+            <div class="btn-wrapper d-flex flex-column justify-content-around align-items-center">
+                <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="VisibilitySwitch1">
+                    <label class="custom-control-label" for="visibilitySwitch1">Set Visibility</label>
+                </div>
+                <div>
+                    <button href=" {{ route('apt-edit', $apt -> id) }}" type="button" class="btn btn-primary btn-sm m-1">Edit</button>
+                    <button type="button" class="btn btn-danger btn-sm m-1">DELETE</button>
+                </div>
+            </div>
+        </div>
+        @endforeach
+            
+        {{-- <div class="list-group">
 
             @foreach($apts as $apt)
-         
+            
                 <div class="list-group-item">
 
                 <a href="{{ route('apt-show', $apt -> id) }}" >{{ $apt -> title}}</a>
@@ -19,16 +52,11 @@
                 <a class="btn btn-sm btn-danger float-right mr-2" href="{{route('apt-delete', $apt -> id)}}">DELETE</a>
 
                 </div>
-
-
         
             @endforeach
 
-
-        </div>
+        </div> --}}
     </div>
 </div>
-
-
 @endsection
 
