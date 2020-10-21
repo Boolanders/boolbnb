b48dd3ee69ba0b7e78e763b05aedbb4998ab8f7c
Michele Curtaz <mcurtaz@gmail.com>
2020-10-21 15:00:26 +0200

    creati link

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,32 +10,38 @@
         </div>
         @foreach ($apts as $apt)
         <div class="media border rounded m-2 p-2 ">
-            @if ($apt -> images -> first -> img -> img)
+                @if ($apt -> images -> first -> img -> img)
 
-            <img src=" {{ $apt -> images -> first -> img -> img }} " class="mr-3 center-cropped align-self-center" alt="...">
+                <img href="{{ route('apt-show', $apt -> id) }}" src=" {{ $apt -> images -> first -> img -> img }} " class="mr-3 center-cropped align-self-center" alt="show">
 
-            @else
+                @else
 
-            <img src=" {{ asset('img/image-not-found.png') }} " class="mr-3 center-cropped align-self-center" alt="...">
+                <img href="{{ route('apt-show', $apt -> id) }}" src=" {{ asset('img/image-not-found.png') }} " class="mr-3 center-cropped align-self-center" alt="show">
 
-            @endif
+                @endif
 
             <div class="media-body">
-              <h3 class="mt-0 mr-2"> {{ $apt -> title }} </h3>
-              <div class="txt mr-2">
-                  {{ $apt -> description }}
-              </div>
+                <a href=" {{ route('apt-show', $apt -> id) }} ">
+                    <h3 class="mt-0 mr-2"> {{ $apt -> title }} </h3>
+                </a>
+                <div class="txt mr-2">
+                      {{ $apt -> description }}
+                </div>
             </div>
+            
             <div class="btn-wrapper d-flex flex-column justify-content-around align-items-center">
+
                 <div class="custom-control custom-switch">
                     <input type="checkbox" class="custom-control-input" id="VisibilitySwitch1">
                     <label class="custom-control-label" for="visibilitySwitch1">Set Visibility</label>
                 </div>
+
                 <div>
                     <button href=" {{ route('apt-edit', $apt -> id) }}" type="button" class="btn btn-primary btn-sm m-1">Edit</button>
                     <button type="button" class="btn btn-danger btn-sm m-1">DELETE</button>
                 </div>
             </div>
+
         </div>
         @endforeach
             
