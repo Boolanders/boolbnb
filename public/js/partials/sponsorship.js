1a009c078adf435da112fcec735cd01ef54eff97
Michele Curtaz <mcurtaz@gmail.com>
2020-10-29 22:38:19 +0100

    sponsorship graphics adjustment

--- public/js/partials/sponsorship.js
+++ public/js/partials/sponsorship.js
@@ -10983,11 +10983,32 @@ $(document).ready(sponsorship);
 function sponsorship() {
   $('.choose').on('click', function () {
     var id = $(this).data('id');
-    var target = $('#promo-id');
-    target.val(id);
-    console.log($('input#promo-id').val());
+    $('.card.promo').removeClass('fixed-hover');
+    $(this).parents('.card.promo').addClass('fixed-hover');
+    var targetId = $('#promo-id');
+    targetId.val(id);
+    var targetPrice = $('#details-price');
+    var targetDate = $('#details-ending');
+    var price = $(this).parents('.promo').data('price');
+    var hours = $(this).parents('.promo').data('hours');
+    var endDate = new Date();
+    endDate.setHours(endDate.getHours() + hours);
+    var formattedEndDate = appendLeadingZeroes(endDate.getDate()) + "/" + appendLeadingZeroes(endDate.getMonth() + 1) + "/" + endDate.getFullYear() + " " + appendLeadingZeroes(endDate.getHours()) + ":" + appendLeadingZeroes(endDate.getMinutes());
+    targetPrice.text(price);
+    targetDate.text(formattedEndDate);
+    $('#order-container').removeClass('d-none');
   });
 }
+
+function appendLeadingZeroes(n) {
+  if (n <= 9) {
+    return "0" + n;
+  }
+
+  return n;
+}
+
+;
 
 /***/ }),
 
--- resources/js/partials/sponsorship.js
+++ resources/js/partials/sponsorship.js
@@ -5,12 +5,43 @@ $(document).ready(sponsorship);
 function sponsorship(){
     
     $('.choose').on('click',function(){
+
         var id = $(this).data('id');
 
-        var target = $('#promo-id');
+        $('.card.promo').removeClass('fixed-hover');
 
-        target.val(id);
+        $(this).parents('.card.promo').addClass('fixed-hover');
 
-        console.log($('input#promo-id').val());
+        var targetId = $('#promo-id');
+
+        targetId.val(id);
+        
+        var targetPrice = $('#details-price');
+
+        var targetDate = $('#details-ending');
+
+        var price = $(this).parents('.promo').data('price');
+
+        var hours = $(this).parents('.promo').data('hours');
+
+        
+        var endDate = new Date();
+
+        endDate.setHours( endDate.getHours() + hours );
+
+        var formattedEndDate =  appendLeadingZeroes(endDate.getDate()) + "/" +  appendLeadingZeroes(endDate.getMonth() + 1) + "/" + endDate.getFullYear() + " " +  appendLeadingZeroes(endDate.getHours()) + ":" +  appendLeadingZeroes(endDate.getMinutes());
+        
+        targetPrice.text(price);
+        targetDate.text(formattedEndDate);
+
+        $('#order-container').removeClass('d-none');
+        
     });
 }
+
+function appendLeadingZeroes(n){
+    if(n <= 9){
+      return "0" + n;
+    }
+    return n;
+};
