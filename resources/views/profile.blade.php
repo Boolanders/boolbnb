61c9f75f4f9361977853433d118d2c875836a34d
Michele Curtaz <mcurtaz@gmail.com>
2020-11-02 11:37:59 +0100

    dismissable alerts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,6 +11,9 @@
     @if (session('status'))
     <div class="alert alert-success alert-dismissible fade show">
         {{ session('status') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
     </div>
     @endif
     <div class="row">
--- resources/views/sponsorship.blade.php
+++ resources/views/sponsorship.blade.php
@@ -12,6 +12,9 @@
             @if (session('error'))
                 <div class="alert alert-danger alert-dismissible fade show">
                     {!! session('error') !!}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
             @endif
             @if ($apt -> sponsored)
