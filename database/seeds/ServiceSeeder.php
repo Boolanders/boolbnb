616960228b9d3de62455ce9559f23ecbda038a8a
Michele Curtaz <mcurtaz@gmail.com>
2020-11-03 13:35:25 +0100

    seeder updates -- only services and promotion seed

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -36,11 +36,12 @@ class ServiceSeeder extends Seeder
 
         foreach ($services as $srv) {
 
-            $apt = Apartment::inRandomOrder() -> take(rand(0,5)) -> get();
-
+            
             $newRecord = Service::create($srv);
+            
+            //$apt = Apartment::inRandomOrder() -> take(rand(0,5)) -> get();
 
-            $newRecord -> apartments() -> attach($apt);
+            //$newRecord -> apartments() -> attach($apt);
         }
         
     }
