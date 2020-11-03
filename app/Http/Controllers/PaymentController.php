60e23a2046b06f77fd4c7a2e5dfb34736c512a5b
Michele Curtaz <mcurtaz@gmail.com>
2020-11-03 11:58:39 +0100

    comments on paymentController

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,8 +13,13 @@ use Illuminate\Support\Facades\Auth;
 class PaymentController extends Controller
 {
 
-    public function sponsorshipPayment(Request $request, $id){ //id del Flat
+    // questo controller si occupa della gestione dei pagamenti attraverso Braintree.
+    // Braintree lavora in due fasi. crea un form in cui prede e verifica il metodo di pagamento inserito dall'utente (nel nostro caso è possibile pagare solo con carta di credito.) in un primo momento verifica siano effettivamente i numeri di una carta di credito e questo è gestito in automatico nel frontend.
+    // in un secondo momento addebita sulla carta il prezzo e questa parte viene gestita in questo controller (per ora ovviamente è tutta una simulazione)
+    // carte di test: valida : 4111 1111 1111 1111  -- non valida: 4111 4111 4111 4111111
+    public function sponsorshipPayment(Request $request, $id){
     
+        // il gateway è un oggetto di braintree nella construct prende dall'env i dati del mio account su braintree
         $gateway = new Gateway([
         'environment' => env('BRAINTREE_ENV'),
         'merchantId' => env('BRAINTREE_MERCHANT_ID'),
@@ -22,6 +27,7 @@ class PaymentController extends Controller
         'privateKey' => env('BRAINTREE_PRIVATE_KEY')
         ]);
 
+        // dal form prendiamo quale promotion è stata selezionata. Da quello prendiamo il costo e la durata della promozione.
         $promoId = $request['promo_id'];
 
         $promo = Promotion::where('id', '=', $promoId) -> first();
@@ -30,11 +36,11 @@ class PaymentController extends Controller
 
         $hours = $promo['hours'];
 
-        // Metodo di pagamento
+        // nel form prendiamo anche i dati (creati da braintree) del metodo di pagamento (in questo caso saranno tutti i dati della carta di credito inserita)
         $nonce = $request->payment_method_nonce;
 
 
-        // Passare tutti i dati relativi al pagamento
+        // attraverso gateway -> transaction -> sale implementiamo il pagamento. gli passiamo il costo da addebitare preso dalla promotion selezionata e i dati del metodo di pagamento $nonce
         $result = $gateway->transaction()->sale([
             'amount' => $price,
             'paymentMethodNonce' => $nonce,
@@ -43,9 +49,10 @@ class PaymentController extends Controller
             ]
         ]);
 
-        // Se la transazione ha avuto successo
+        // Nella variabile result troviamo il risultato della transazione.
         if ($result->success || !is_null($result->transaction)) {
             
+            // se è andato a buon fine prendiamo dal form la data di inizio della promozione (che può essere adesso o eventualmente la data di fine della promozione precedente ancora attiva). Ci aggiungo le ore per cui l'utente ha pagato prese dalla promozione selezionata
             $endDate = Carbon::parse($request['start_date']) -> addHours($hours);
 
             $startDate = $request['start_date'] ;
@@ -60,16 +67,20 @@ class PaymentController extends Controller
 
             $usrId = Auth::user() -> id;
 
+            // restitutisco la view della pagina profilo con il messaggio di andata a buon fine dell'operazione
             return redirect() -> route('profile', $usrId)-> with('status', 'Sponsorship created successfully');
 
       
         }else {
+
+            // se la transazione non va a buon fine nella variabile errors creo una stringa con tutti i messaggi di errore contenuti in $result
             $errors = "";
 
             foreach($result->errors->deepAll() as $error) {
                 $errors .= 'Error: ' . $error->code . ": " . $error->message . "<br>";
             }
 
+            // con return back restituisco la pagina di sponsorizzazione dell'appartamento con il messaggio di errore
             return back()-> with('error', $errors);
         }
     }
